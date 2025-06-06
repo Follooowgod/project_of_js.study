e6d30daded4b0369aa67720be4e1c624913c4fad
TSOI S. <followgod@192.168.0.106>
2025-06-06 14:01:07 +0300

    practice_lesson_number_2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 'use strict'
 
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+if (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+  numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+}
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -10,12 +13,34 @@ const personalMovieDB = {
   privat: false
 };
 
-let a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько вы его оцените?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько вы его оцените?', ''); 
+if (personalMovieDB.count < 10) {
+  console.log('Просмотрено довольно мало фильмов');
+}
+else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log('Вы классический зритель');
+}
+else if (personalMovieDB.count >= 30) {
+  console.log('Вы киноман');
+}
+else {
+  console.log('Произошла ошибка');
+} 
+
+for (let i = 0; i < 2; i++) {
+  const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько вы его оцените?', '');
+
+  if (a != null && b != null && a !== '' && b !== '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
+}
+
 
 personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
+
